a2060af965528754bfdf7afd2229b5776ef62887
radohingis <radoslav.hingis@kosickaakademia.sk>
2021-02-20 12:06:06 +0100

    fx init

--- src/sk/kosickaakademia/hingis/netcoffee/Controller.java
+++ src/sk/kosickaakademia/hingis/netcoffee/Controller.java
@@ -1,23 +1,19 @@
 package sk.kosickaakademia.hingis.netcoffee;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Controller {
 
-    private void openMainForm() {
-        try{
-            Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-            Stage stage = new Stage();
-            stage.setTitle("Welcome to the NetCoffee");
-            stage.setScene(new Scene(root, 1024, 768));
-            stage.show();
-        }  catch (IOException e) {
-            e.printStackTrace();
-        }
+    public Button sign_up_button;
+
+    public void openSignUpForm(ActionEvent actionEvent) {
+
     }
 }
--- src/sk/kosickaakademia/hingis/netcoffee/Main.java
+++ src/sk/kosickaakademia/hingis/netcoffee/Main.java
@@ -5,23 +5,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import sk.kosickaakademia.hingis.netcoffee.database.NCSql;
-
-import java.sql.SQLException;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("LoginForm.fxml"));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
-        NCSql dat = new NCSql();
-        dat.getMyMessages("kristianS");
+        launch(args);
     }
 }
